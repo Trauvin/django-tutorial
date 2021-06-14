58b23d275da36203bf9baab48d42042cbb2fc642
Trauvin <mateusgalote@gmail.com>
2021-06-14 16:12:53 -0300

    models and interactive shell explanation

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ToDoList, Item
 
 
 # Create your views here.
 
-def index(response):
-	return HttpResponse("<h1>Hello Quengas</h1>")
-
-def v1(response):
-	return HttpResponse("<h1>view 1</h1>")
+def index(response, name):
+	ls = ToDoList.objects.get(name=name) # acessando os objetos criados no shell
+	items = ls.item_set.get(id=2)
+	return HttpResponse("<h1>%s</h1><br></br><p>%s</p>" %(ls.name, str(items.text)))

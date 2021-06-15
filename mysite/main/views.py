d5972f7c8b1e0b517feb2bfeedd78685a8284ef2
Trauvin <mateusgalote@gmail.com>
2021-06-15 16:45:02 -0300

    templates & custom HTML

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -5,7 +5,9 @@ from .models import ToDoList, Item
 
 # Create your views here.
 
-def index(response, name):
-	ls = ToDoList.objects.get(name=name) # acessando os objetos criados no shell
-	items = ls.item_set.get(id=2)
-	return HttpResponse("<h1>%s</h1><br></br><p>%s</p>" %(ls.name, str(items.text)))
+def index(response, id):
+	ls = ToDoList.objects.get(id=id) # acessando os objetos criados no shell
+	return render(response, "main/list.html", {"ls": ls})
+
+def home(response):
+	return render(response, "main/home.html", {})

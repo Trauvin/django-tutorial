dde4681e4c4c6c7d45568d56973449dcfb2da523
Trauvin <mateusgalote@gmail.com>
2021-06-16 16:55:27 -0300

    custom forms

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -2,12 +2,30 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import ToDoList, Item
 from .forms import CreateNewList
-
-
 # Create your views here.
 
 def index(response, id):
 	ls = ToDoList.objects.get(id=id) # acessando os objetos criados no shell
+
+	if response.method == "POST":
+		print(response.POST)
+		if response.POST.get("save"):
+			for item in ls.item_set.all():
+				if response.POST.get("c" + str(item.id)) == "clicked":
+					item.complete = True
+				else:
+					item.complete = False
+
+				item.save()
+
+		elif response.POST.get("newItem"):
+			txt = response.POST.get("new")
+
+			if len(txt) > 2:
+				ls.item_set.create(text=txt, complete=False)
+			else:
+				print("invalid")
+
 	return render(response, "main/list.html", {"ls": ls})
 
 def home(response):
